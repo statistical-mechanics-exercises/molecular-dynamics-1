457c799da4066b654ece26789638a0a024285000
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 17:08:45 +0000

    Added answers

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import numpy as np
 def potential(x) :
   energy=0
   # Your code to calculate the potential goes here
-  
+  for i in range(1,7) :
+      for j in range(i) :
+          diff = x[i,:] - x[j,:]
+          d2 = sum(diff*diff)
+          d6 = d2*d2*d2
+          energy = energy + 4/(d6*d6) - 4/d6   
   return energy
   
 # This command reads in the positions that are contained in the file called positions.txt
